09120cc0a899661ccf9e0f0274172d11afc29f96
Mitar <mitar.git@tnode.com>
2016-01-20 09:37:11 -0800

    Version bump.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Reactive publish endpoints",
-  version: '0.1.3',
+  version: '0.2.0',
   name: 'peerlibrary:reactive-publish',
   git: 'https://github.com/peerlibrary/meteor-reactive-publish.git'
 });
